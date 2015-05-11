40998eadb705d69b84092f7600b61e71249c2c00
Aaron Fargose <aaron@kodeplay.com>
2015-05-11 14:42:31 +0530

    added default configs

---
+++ admin/config_dist.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://storebuilder.kp/admin/');
+define('HTTP_CATALOG', 'http://storebuilder.kp/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://storebuilder.kp/admin/');
+define('HTTPS_CATALOG', 'http://storebuilder.kp/');
+
+// DIR
+define('DIR_APPLICATION', '/home/aaron/public_html/storebuilder/admin/');
+define('DIR_SYSTEM', '/home/aaron/public_html/storebuilder/system/');
+define('DIR_LANGUAGE', '/home/aaron/public_html/storebuilder/admin/language/');
+define('DIR_TEMPLATE', '/home/aaron/public_html/storebuilder/admin/view/template/');
+define('DIR_CONFIG', '/home/aaron/public_html/storebuilder/system/config/');
+define('DIR_IMAGE', '/home/aaron/public_html/storebuilder/image/');
+define('DIR_CACHE', '/home/aaron/public_html/storebuilder/system/cache/');
+define('DIR_DOWNLOAD', '/home/aaron/public_html/storebuilder/system/download/');
+define('DIR_UPLOAD', '/home/aaron/public_html/storebuilder/system/upload/');
+define('DIR_LOGS', '/home/aaron/public_html/storebuilder/system/logs/');
+define('DIR_MODIFICATION', '/home/aaron/public_html/storebuilder/system/modification/');
+define('DIR_CATALOG', '/home/aaron/public_html/storebuilder/catalog/');
+
+define('DNS_POINTING', 'storebuilder.kp'); //todo
+define('DOMAIN','storebuilder.kp');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'storebuilder');
+define('DB_PREFIX', '');
---
+++ config_dist.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://storebuilder.kp/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://storebuilder.kp/');
+
+// DIR
+define('DIR_APPLICATION', '/home/aaron/public_html/storebuilder/catalog/');
+define('DIR_SYSTEM', '/home/aaron/public_html/storebuilder/system/');
+define('DIR_LANGUAGE', '/home/aaron/public_html/storebuilder/catalog/language/');
+define('DIR_TEMPLATE', '/home/aaron/public_html/storebuilder/catalog/view/theme/');
+define('DIR_CONFIG', '/home/aaron/public_html/storebuilder/system/config/');
+define('DIR_IMAGE', '/home/aaron/public_html/storebuilder/image/');
+define('DIR_CACHE', '/home/aaron/public_html/storebuilder/system/cache/');
+define('DIR_DOWNLOAD', '/home/aaron/public_html/storebuilder/system/download/');
+define('DIR_UPLOAD', '/home/aaron/public_html/storebuilder/system/upload/');
+define('DIR_MODIFICATION', '/home/aaron/public_html/storebuilder/system/modification/');
+define('DIR_LOGS', '/home/aaron/public_html/storebuilder/system/logs/');
+
+define('DEFAULT_DB', 'storebuilder.sql');
+
+define('DNS_POINTING', 'storebuilder.kp');
+define('DOMAIN', 'storebuilder.kp');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'storebuilder');
+define('DB_PREFIX', '');
